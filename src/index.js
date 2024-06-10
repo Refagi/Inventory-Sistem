fc875893dcafde05ea5e92b9951a8c7ffff46189
Refagi <renogivaro@gmail.com>
2024-06-10 18:01:54 +0700

    change configuration before deploying to vercel to fix error

--- src/index.js
+++ src/index.js
@@ -6,21 +6,31 @@ const prisma = require('../prisma/index');
 
 let server;
 
-if(prisma){
-  logger.info('Connected to Database');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port}`);
-  });
+// Attempt to connect to the database and start the server
+async function startServer() {
+  try {
+    await prisma.$connect();
+    logger.info('Connected to Database');
+
+    server = app.listen(config.port, () => {
+      logger.info(`Listening to port ${config.port}`);
+    });
+  } catch (error) {
+    logger.error('Failed to connect to the database:', error);
+    process.exit(1); // Exit with an error code
+  }
 }
+
+startServer();
 
 const exitHandler = () => {
   if (server) {
     server.close(() => {
       logger.info('Server closed');
-      process.exit(1);
+      process.exit(0); // Exit normally
     });
   } else {
-    process.exit(1);
+    process.exit(0); // Exit normally
   }
 };
 
